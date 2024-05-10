3b32f4f4eddc9194880c1b40e461b2b3e7801aef
Sergent2512 <kajanansayanthan@gmail.com>
2024-05-10 20:13:58 +0000

    Exercise 1

--- exercises/exercise_1.py
+++ exercises/exercise_1.py
@@ -1,2 +1,8 @@
-# Exercise 1
-# Your solution comes here
+num = int(input("Enter a number : "))
+
+sum1 = int(str(num)[0]) + int(str(num)[2]) + int(str(num)[4])
+sum2 = int(str(num)[1]) + int(str(num)[3])
+
+Summation = int((str(sum1) + str(sum2)))
+
+print(Summation)
