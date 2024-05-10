8a6ed90d0ff81797cc25ad75ec4a2b5400543908
Sergent2512 <kajanansayanthan@gmail.com>
2024-05-10 20:16:42 +0000

    Exercise 3

--- exercises/exercise_3.py
+++ exercises/exercise_3.py
@@ -1,2 +1,13 @@
-# Exercise 3
-# Your solution comes here
+n = int(input("Enter the number of seconds. : "))
+
+hours = n // 3600
+
+seconds_left = n % 3600
+
+minutes = seconds_left // 60
+
+seconds = seconds_left % 60
+
+time = "{}:{}:{}".format(hours, minutes, seconds)
+
+print(time)
