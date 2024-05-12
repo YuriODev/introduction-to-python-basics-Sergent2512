6b771f163ee5611612b40f79632a754c931af72a
Sergent2512 <kajanansayanthan@gmail.com>
2024-05-12 07:28:09 +0000

    Exercise 9

--- exercises/exercise_9.py
+++ exercises/exercise_9.py
@@ -1,2 +1,8 @@
-# Exercise 9
-# Your solution comes here
+hours = int(input("Value between 0 <= h <= 12 : "))
+minutes = int(input("Value between 0 <= m <= 60 : "))
+seconds = int(input("Value between 0<= s <= 60 : "))
+
+total_mins = (hours * 60) + minutes + (seconds/60)
+degrees = (((total_mins * 6 ) / 360) / 12) * 360
+
+print(degrees, "°")
