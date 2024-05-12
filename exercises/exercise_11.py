0c70cbb74041ff4d4205b72782867dc1f0362010
Sergent2512 <kajanansayanthan@gmail.com>
2024-05-12 07:52:20 +0000

    Exercise 11

--- exercises/exercise_11.py
+++ exercises/exercise_11.py
@@ -1,2 +1,7 @@
-# Exercise 11
-# Your solution comes here
+denominations = [500, 100, 10, 5, 2, 1] # 2804
+s = int(input("Enter the value of your shopping $ : "))
+
+for denomination in denominations:
+    number = s// denomination
+    print("The number of", denomination , "bills needed is :" , number)
+    s %= denomination
